1b1af643394447ce1288c4c1f81b859a2fce66c2
Ross <44214623+Rbbradle@users.noreply.github.com>
2018-11-06 18:52:14 -0500

    Add files via upload

---
+++ exercise 2 script.R
@@ -0,0 +1,46 @@
+library (tidyverse)
+titanic <- read_csv("titanic_original.csv")
+head(titanic)
+str(titanic)
+titanic$embarked
+
+
+#Port of embarkation - Replace Blank cells with "S"
+
+titanic %>% mutate(embarked = gsub("^$|^ $", "S", embarked) %>%
+                     gsub("NA", "S", .))
+
+
+#Age - Populate missing values with mean of age column.
+unique(titanic$age)
+
+#Mean Age
+mean_age <- mean(titanic$age, na.rm = TRUE)
+print(mean_age)
+
+#Replace NA values with mean age
+titanic$age[is.na(titanic$age)]<- mean_age
+print(mean_age)
+is.na(titanic$age) %>% sum()
+titanic$age[1:15]
+titanic$age[c(TRUE, FALSE)]
+ 
+#Lifeboat
+
+titanic$boat <- gsub("", NA , titanic$boat)
+table(titanic$boat)
+isTRUE(TRUE)
+
+
+#Cabin
+
+titanic <- titanic %>% mutate(has_cabin_number = ifelse(is.na(cabin),0,1))
+head(titanic)
+titanic$has_cabin_number
+
+is.null(titanic$cabin) %>% sum()
+is.na(titanic$cabin) %>% sum()
+nrow(titanic)
+table(titanic$cabin)   
+
+View(titanic)
